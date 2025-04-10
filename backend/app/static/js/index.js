2c6194279b5fa75cb89a76941d08d370a4d6cb6d
iamchiragchopra@gmail.com <iamchiragchopra@gmail.com>
2025-04-11 01:13:02 +0530

    UI changes

--- backend/app/static/js/index.js
+++ backend/app/static/js/index.js
@@ -1,28 +1,29 @@
 let currentLanguage = "en";
 
 function scrollToTop() {
-	window.scrollTo({top: 0, behavior: "smooth"});
+  window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
 function toggleLanguage() {
-	currentLanguage = document.getElementById("languageToggle").checked ? "ar" : "en";
+  currentLanguage = document.getElementById("languageToggle").checked ? "ar" : "en";
 
-	let questionSection = document.getElementById("questions");
-	if (questionSection) {
-		questionSection.classList.toggle("rtl", currentLanguage === "ar");
-		document.documentElement.setAttribute("dir", currentLanguage === "ar" ? "rtl" : "ltr");
-	}
+  let questionSection = document.getElementById("questions");
 
-	let feedbackSection = document.querySelector(".feedback-section");
-	if (feedbackSection) {
-		feedbackSection.classList.toggle("rtl", currentLanguage === "ar");
-	}
+  if (questionSection) {
+    questionSection.classList.toggle("rtl", currentLanguage === "ar");
+    document.documentElement.setAttribute("dir", currentLanguage === "ar" ? "rtl" : "ltr");
+  }
 
-	if (document.getElementById("questionnaire")) {
-		loadSection();
-	}
+  let feedbackSection = document.querySelector(".feedback-section");
+  if (feedbackSection) {
+    feedbackSection.classList.toggle("rtl", currentLanguage === "ar");
+  }
 
-	if (document.getElementById("feedbackContent")) {
-		loadFeedbackPage();
-	}
+  if (document.getElementById("questionnaire")) {
+    loadSection();
+  }
+
+  if (document.getElementById("feedbackContent")) {
+    loadFeedbackPage();
+  }
 }
